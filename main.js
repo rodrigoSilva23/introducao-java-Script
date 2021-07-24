729b5e53129efa0a00e632459dee719ccd9bf1d7
rodrigoSilva23 <rodriguinho2456@gmail.com>
2021-07-24 19:03:51 -0300

    concluida

--- main.js
+++ main.js
@@ -15,14 +15,14 @@ fruts.forEach(element => {
 //dicionario
 
 var frutas = [{ cor: "amarela", fruta: "manga", tipo: "fruta" }, { cor: "verde", fruta: "laranja", tipo: "fruta" }, { cor: "vermelha", fruta: "banana", tipo: "fruta" }, { cor: "azul", fruta: "maçã", tipo: "fruta" }]
-/*console.log(frutas[0].cor);
+console.log(frutas[0].cor);
 frutas.forEach(element =>{
     if (element.cor == "verde" || element.cor == "amarela") {
         console.log(element);
         console.log(`as frutas selecionadas são ${element.fruta}?`)
     }
     
-})*/
+})
 
 //while
 
@@ -35,4 +35,26 @@ while (count <= frutas.length ){
 
     
     count++;
+}
+/*n1 = prompt("Digite um numero");
+n2 = prompt("Digite outro numero");
+function soma (a, b){
+    return parseFloat(a) + parseFloat(b);
+}
+alert("a soma foi"+soma(n1,n2));*/
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+}
+function passou(element){
+    element.innerHTML = "Obrigado por passar o mause!!!!"
+}
+
+function voltar(element){
+    element.innerHTML = "passe o mause aqui"
+
+}
+
+function loanding(){
+    alert("Pagina carregada!!");
 }
